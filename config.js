07ee3def96124bb2fddb45549432e490b2bc4529
Ahsan-ali001 <ahsanali45444@gmail.com>
2023-11-03 11:38:47 -0700

    creat file store

--- config.js
+++ config.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
     apiKey: "AIzaSyA0asL3eKhMlZkbK3qcuSxJd22pWFXL2Fk",
@@ -14,3 +15,4 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
--- home.js.js
+++ home.js.js
@@ -1,43 +1,93 @@
 import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
-import { auth } from "./config.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 
-
+// on auth start
 onAuthStateChanged(auth, (user) => {
-    if (user) {
-      const uid = user.uid;
-      console.log(uid);
-      const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        didOpen: (toast) => {
-          toast.addEventListener('mouseenter', Swal.stopTimer)
-          toast.addEventListener('mouseleave', Swal.resumeTimer)
-        }
-      })
-      
-      Toast.fire({
-        icon: 'success',
-        title: 'Login in successfully'
-      })
+  if (user) {
+    const uid = user.uid;
+    console.log(uid);
+    const Toast = Swal.mixin({
+      toast: true,
+      position: 'top-end',
+      showConfirmButton: false,
+      timer: 3000,
+      timerProgressBar: true,
+      didOpen: (toast) => {
+        toast.addEventListener('mouseenter', Swal.stopTimer)
+        toast.addEventListener('mouseleave', Swal.resumeTimer)
+      }
+    })
 
-    } else {
-        window.location='./login.html'
-    }
+    Toast.fire({
+      icon: 'success',
+      title: 'Login in successfully'
+    })
+
+  } else {
+    window.location = './login.html'
+  }
+});
+
+const logout = document.querySelector('.btn');
+logout.addEventListener('click', () => {
+  signOut(auth).then(() => {
+    console.log('logout successfully');
+
+    window.location = 'login.html'
+  }).catch((error) => {
+    console.log(error);
   });
+})
+// on auth end
 
-  const logout = document.querySelector('.btn');
-  logout.addEventListener('click',()=>{
-    signOut(auth).then(() => {
-        console.log('logout successfully');
-        
-        window.location = 'login.html'
-      }).catch((error) => {
-        console.log(error);
-      });
-  })
+// html id come
+const form = document.querySelector('#form')
+const title = document.querySelector('#title')
+const des = document.querySelector('#description')
+const card = document.querySelector('#card');
+// html id stop
+const arry = []
 
+// arry push function start
+async function getDataFromFirestore() {
   
+  const querySnapshot = await getDocs(collection(db, "posts"));
+  querySnapshot.forEach((doc) => {
+    arry.push(doc.data());
+  });
+  console.log(arry);
+  arry.map((item) => {
+    card.innerHTML += `
+    <div class="card">
+    <div class="card-body">
+    <p><span class="h4">Title : </span> <span class="main-t">${item.Title}</span></p>
+    <p><span class="h4">Description : </span> <span class="main-d">${item.Description}</span> </p>
+    </div>
+    </div>`
+  })
+}
+// arry push function end
+getDataFromFirestore()
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  card.innerHTML = ''
+  try {
+    const docRef = await addDoc(collection(db, "posts"), {
+      Title: title.value,
+      Description: des.value
+    });
+    title.value=''
+    des.value=''
+    console.log("Document written with ID: ", docRef.id);
+    getDataFromFirestore()
+
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+})
+
+
+
